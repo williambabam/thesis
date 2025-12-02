5cc337f32d2474bd5a428791e925e6d34ff55d4a
williambabam <burgerhatdog69@gmail.com>
2025-12-02 21:03:50 +0800

    Update browse.php

--- browse.php
+++ browse.php
@@ -1,3 +1,4 @@
+//browse.php
 <?php
 declare(strict_types=1);
 require_once __DIR__ . '/../config.php';
@@ -166,4 +167,5 @@ document.addEventListener('DOMContentLoaded', function() {
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
